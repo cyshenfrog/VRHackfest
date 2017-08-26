f39c4fd755d15ae9c3b6e5f4867f8380b880db23
CL Wu <hardcodefool@gmail.com>
2017-08-26 22:27:02 +0800

    choose item bugfix

--- GearController/Assets/Scenes/Scripts/Game.cs
+++ GearController/Assets/Scenes/Scripts/Game.cs
@@ -28,11 +28,25 @@ public class Game : MonoBehaviour
 
     public InteractiveObject[] items;
 
+    public Transform gearVRControllerAnchor;
+    public Transform daydreamControllerAnchor;
+    public Transform controllerAnchor;
+    public Transform reticle;
     public float distanceForwardController = 1;
+    public Transform leverPlane;
+    public Vector3 leverPlaneNormal;
     public Transform lever1, lever2;
+    public Transform lever1End, lever2End;
+    public Transform itemAttachedToLever1, itemAttachedToLever2;
+    public Transform currentItemRaycastHit;
+    public Transform currentRaycastHit;
+
+    public const string CORRECT_ITEM = "CorrectItem";
+    public const string WRONG_ITEM = "WrongItem";
 
     public InteractiveObject interactiveSample;
     public Text info;
+    public LineRenderer line;
 
     private void Awake()
     {
@@ -59,8 +73,16 @@ public class Game : MonoBehaviour
         }
         _controller = ControlManager.Instance;
         _controller.SetupCanvas(canvas);
+        if (_controller.control == ControlManager.Control.GearVR)
+        {
+            controllerAnchor = gearVRControllerAnchor;
+        }
+        else if (_controller.control == ControlManager.Control.Daydream)
+        {
+            controllerAnchor = daydreamControllerAnchor;
+        }
 
-        items[0].onPointerClick = OnItemPointerClick;
+        leverPlaneNormal = leverPlane.forward;
 
         interactiveSample.onPointerClick = (go) => { info.text = "Click"; };
         interactiveSample.onPointerExit = (go) => { info.text = "Exit"; };
@@ -72,8 +94,8 @@ public class Game : MonoBehaviour
         {
             return;
         }
-        Vector3 euler = _controller.Rotation.eulerAngles;
-        lever1.rotation = Quaternion.Euler(euler.x, 0, euler.z);
+
+        ChooseItem();
     }
 
     public void OnStartButtonClicked()
@@ -81,13 +103,83 @@ public class Game : MonoBehaviour
         startButton.gameObject.SetActive(false);
     }
 
-    public void OnItemPointerEnter(GameObject go)
+    private void ChooseItem()
     {
+        Ray ray;
+        if (_controller.control == ControlManager.Control.Daydream)
+        {
+            ray = new Ray(controllerAnchor.position, reticle.position - controllerAnchor.position);
+            line.SetPosition(0, controllerAnchor.position);
+            line.SetPosition(1, reticle.position);
+        }
+        else
+        {
+            ray = new Ray(controllerAnchor.position, controllerAnchor.forward);
+            line.SetPosition(0, controllerAnchor.position);
+            line.SetPosition(1, controllerAnchor.position + controllerAnchor.forward * distanceForwardController);
+        }
+        
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.CompareTag(WRONG_ITEM) || hit.collider.CompareTag(CORRECT_ITEM))
+            {
+                currentItemRaycastHit = hit.collider.transform;
+            }
+            else
+            {
+                currentItemRaycastHit = null;
+            }
+        }
+        else
+        {
+            currentItemRaycastHit = currentRaycastHit = null;
+        }
 
+        Quaternion q = Quaternion.LookRotation(-controllerAnchor.forward);
+        Vector3 original = -controllerAnchor.forward;
+        Vector3 project = original - (Vector3.Dot(original, leverPlaneNormal) / leverPlaneNormal.sqrMagnitude) * leverPlaneNormal;
+        lever1.rotation = lever2.rotation = Quaternion.LookRotation(project);
+
+        if (_controller.SecondaryButtonDown)
+        {
+            //info.text = "SecondaryButtonDown";
+            if (itemAttachedToLever1 == null)
+            {
+                if (currentItemRaycastHit != null)
+                {
+                    info.text = currentItemRaycastHit.name;
+                    currentItemRaycastHit.SetParent(lever1End, true);
+                    currentItemRaycastHit.localPosition = Vector3.zero;
+                    itemAttachedToLever1 = currentItemRaycastHit;
+                }
+                else
+                {
+                    info.text = "?????";
+                }
+            }
+            else
+            {
+                info.text = "Drop " + itemAttachedToLever1.name;
+                itemAttachedToLever1.SetParent(null);
+                itemAttachedToLever1.position = itemAttachedToLever1.GetComponent<InteractiveObject>().originalPosition;
+                itemAttachedToLever1.rotation = itemAttachedToLever1.GetComponent<InteractiveObject>().originalRotation;
+                itemAttachedToLever1 = null;
+            }
+        }
+
+        if (itemAttachedToLever1 != null)
+        {
+            itemAttachedToLever1.rotation = controllerAnchor.rotation;
+        }
+        if (itemAttachedToLever2 != null)
+        {
+            itemAttachedToLever2.rotation = controllerAnchor.rotation;
+        }
     }
 
-    public void OnItemPointerClick(GameObject go)
+    private void Unlock()
     {
-        go.transform.SetParent(lever1.Find("End"), false);
+
     }
 }
---
+++ GearController/Assets/Scenes/Scripts/Lock.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    public Transform[] rings;
+    public float[] targetDegrees;
+    public float[] yDegrees;
+    public int current;
+    public bool solved;
+
+    private void Awake()
+    {
+        yDegrees = new float[rings.Length];
+    }
+
+	private void Start ()
+    {
+        current = 0;
+
+    }
+
+    private void Update ()
+    {
+		
+	}
+
+    public void Slide(float delta)
+    {
+        if (solved)
+        {
+            return;
+        }
+        int len = rings.Length;
+        if (delta > 0.0625f)
+        {
+            if (len == 4)
+            {
+                rings[current].Rotate(Vector3.up, 90, Space.Self);
+                if (rings[current].localEulerAngles.y == targetDegrees[current])
+                {
+                    current++;
+                    if (current >= rings.Length)
+                    {
+                        current = 0;
+                        solved = true;
+                    }
+                }
+            }
+        }
+        else if (delta < -0.0625)
+        {
+            if (len == 4)
+            {
+                rings[current].Rotate(Vector3.up, -90, Space.Self);
+                if (rings[current].localEulerAngles.y == targetDegrees[current])
+                {
+                    current++;
+                    if (current >= rings.Length)
+                    {
+                        current = 0;
+                        solved = true;
+                    }
+                }
+            }
+        }
+    }
+}
